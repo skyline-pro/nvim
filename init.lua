28c6f732844254a43f25292e12dad7f933c9fd29
skyline <zhou_cansong@outlook.com>
2023-09-10 02:11:13 +0800

    restruct file

--- init.lua
+++ init.lua
@@ -1,3 +1,28 @@
 require('options')
 require('keymaps')
-require('plugins')
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+    { -- Theme inspired by Atom
+    'navarasu/onedark.nvim',
+    priority = 1000,
+    config = function()
+      vim.cmd.colorscheme 'onedark'
+    end,
+    },
+
+    {import = "plugins"}
+
+})
